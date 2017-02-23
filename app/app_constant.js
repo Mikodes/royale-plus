810f3a787e4b4d5067e16d0203b1b67b90188d39
Amir Mehdi Savand <amir@savandbros.com>
2017-02-23 14:04:19 +0400

    Add all app constants

---
+++ app/app_constant.js
@@ -0,0 +1,57 @@
+"use strict";
+
+app.constant("Scores", {
+  role: {
+    leader: 350,
+    coleader: 200,
+    elder: 100,
+    member: 0
+  },
+  donation: {
+    s: 200,
+    a: 100,
+    b: 75,
+    c: 50,
+    d: 0
+  },
+  rarity: {
+    legendary: 200,
+    epic: 100,
+    rare: 50,
+    common: 0
+  },
+  tag: 50
+});
+
+app.constant("Tags", {
+  "godlike": {
+    font: "user-secret",
+    sentence: "is the creator of clan."
+  },
+  "creator": {
+    font: "cube",
+    sentence: "is the creator of clan website."
+  },
+  "clan chest master": {
+    font: "trophy",
+    sentence: "won the Clan Chest Master (1st)."
+  },
+  "whatsapp": {
+    font: "whatsapp",
+    sentence: "has joined the clan's WhatsApp group."
+  }
+});
+
+app.constant("Sorts", {
+  rarity: {
+    Common: 0,
+    Rare: 1,
+    Epic: 2,
+    Legendary: 3
+  },
+  type: {
+    Troop: 0,
+    Spell: 1,
+    Building: 2
+  }
+});
