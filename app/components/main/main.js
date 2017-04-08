b0aefe2386e3fdc5710dd1c65e3323d79109e9cf
Amir Mehdi Savand <amir@savandbros.com>
2017-04-08 13:22:54 +0400

    Update version

--- app/components/main/main.js
+++ app/components/main/main.js
@@ -4,6 +4,7 @@ app.controller("MainController", function ($scope, $rootScope, $state, $http) {
 
   function constructor() {
     $scope.state = $state;
+    $scope.version = "0.4.0";
 
     if (!localStorage.cards) {
       $http.get("http://www.clashapi.xyz/api/cards").then(function (data) {
