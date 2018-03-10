13573b7c3e60df5069b8c260f31a3dab9eb30fba
Amir Savand <amir@savandbros.com>
2018-03-10 20:15:30 +0300

    Register ActivityViewSet

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import url
 from rest_framework import routers
 from rest_framework.documentation import include_docs_urls
-
-from api.views import UserViewSet, UserCreateView, UserUpdateView, DeckViewSet
 from rest_framework_jwt.views import obtain_jwt_token
 
+from api.views import UserViewSet, UserCreateView, UserUpdateView, DeckViewSet, ActivityViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'decks', DeckViewSet)
+router.register(r'activities', ActivityViewSet)
 
 urlpatterns = router.urls
 urlpatterns += [
