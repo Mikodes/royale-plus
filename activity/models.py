78c090350977aea15cd1816d3e1ff932429ea5fa
Amir Savand <amir@savandbros.com>
2018-03-10 16:29:29 +0300

    Add activity model

--- activity/models.py
+++ activity/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from account.models import User
+
+ACTIVITY_TYPE_CHOICES = (
+    (0, 'None'),
+    (1, 'Update'),
+    (2, 'User'),
+    (3, 'Deck'),
+)
+
+
+class Activity(models.Model):
+    type = models.IntegerField(choices=ACTIVITY_TYPE_CHOICES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        verbose_name = 'Activity'
+        verbose_name_plural = 'Activities'
+        get_latest_by = 'date_created'
+        ordering = ['-date_created', ]
