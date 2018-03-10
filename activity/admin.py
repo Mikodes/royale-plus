79b6790fbc7f677f84671a4d2749c4bf39fb4ffe
Amir Savand <amir@savandbros.com>
2018-03-10 20:14:31 +0300

    Add activity to admin

--- activity/admin.py
+++ activity/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from activity.models import Activity
+
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = (
+        'issuer',
+        'kind',
+        'content',
+        'created',
+    )
+    list_filter = [
+        'issuer',
+        'kind',
+        'created',
+    ]
