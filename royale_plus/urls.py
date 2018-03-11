3b02c6ffcc925877ac355244b741e54eb08431c6
Amir Savand <amir@savandbros.com>
2018-03-11 22:31:47 +0300

    Use env variable for admin URL

--- royale_plus/urls.py
+++ royale_plus/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 from api.urls import router
+from royale_plus.settings import ADMIN_URL
 
 urlpatterns = (
-    url(r'^admin/', admin.site.urls),
+    url(ADMIN_URL, admin.site.urls),
     url(r'^crapi/', include('crapi.urls')),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls')),
