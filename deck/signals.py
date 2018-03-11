8eefcda2a267592bcc6a7e80f9149245c21ad648
Amir Savand <amir@savandbros.com>
2018-03-11 03:35:46 +0300

    Create activity when a deck created

---
+++ deck/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from deck.models import Deck
+from activity.models import Activity
+
+
+@receiver(post_save, sender=Deck)
+def create_user(sender, instance, created, **kwargs):
+    if created:
+        Activity.objects.create(
+            issuer=instance.user,
+            content=f'built a {instance.avg_elixir} elixir deck',
+            kind=3,
+        )
