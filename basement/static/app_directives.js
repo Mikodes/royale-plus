918e2064453e507ae1fc790828663d4348354ea2
Amir Savand <amir@savandbros.com>
2018-03-13 20:00:32 +0300

    Add directive for buttons-checkbox

--- basement/static/app_directives.js
+++ basement/static/app_directives.js
@@ -13,3 +13,28 @@ app.directive("onload", ["$parse", function ($parse) {
     }
   };
 }]);
+
+app.directive("input", function () {
+  return {
+    restrict: "E",
+    require: "ngModel",
+    link: function ($scope, element, attr, ctrl) {
+      // Ignore other kind of button groups (e.g. buttons-radio)
+      if (!element.parent("[data-toggle='buttons-checkbox'].btn-group").length) {
+        return;
+      }
+      // Set/unset active class when model changes
+      $scope.$watch(attr.ngModel, function (newValue, oldValue) {
+        element.toggleClass("active", ctrl.$viewValue);
+      });
+      // update model when button is clicked
+      element.bind("click", function (e) {
+        $scope.$apply(function (scope) {
+          ctrl.$setViewValue(!ctrl.$viewValue);
+        });
+        // Don't let Bootstrap.js catch this event as we are overriding its data-toggle behavior.
+        e.stopPropagation();
+      });
+    }
+  };
+});
