33a5d3b4cd96c40cf2645b7977ca92763358b2e0
Amir Savand <amir@savandbros.com>
2018-03-15 21:51:30 +0300

    Add Comment.create()

--- basement/static/components/comment/comment.js
+++ basement/static/components/comment/comment.js
@@ -14,6 +14,22 @@ app.service("Comment", function (Main) {
      */
     this.getKind = function () {
       return Main.comment.kind[this.get.kind];
+    this.create = function () {
+      API.Comments.save({
+          kind: this.kind,
+          object: this.object,
+          comment: this.comment
+        },
+        function (data) {
+          $rootScope("royalePlus.Comment:create", data);
+          toaster("Done", "Succesfully commented.");
+          this.loading = false;
+        },
+        function () {
+          toaster("Ops", "Failed to comment, try again later.");
+          this.loading = false;
+        }
+      );
     };
   };
 });
