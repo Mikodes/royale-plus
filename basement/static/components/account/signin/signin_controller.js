9be44d2249e8458f65db994df48177f67d816540
Amir Savand <amir@savandbros.com>
2018-03-16 19:06:12 +0300

    Fix sign in form

--- basement/static/components/account/signin/signin_controller.js
+++ basement/static/components/account/signin/signin_controller.js
@@ -1,21 +1,11 @@
 "use strict";
 
-app.controller("SignInController", function (Auth, API, toaster, $scope, $state) {
+app.controller("SignInController", function (Auth, $scope) {
 
-  this.signIn = function (form) {
-    form.loading = true;
-    console.log("ss");
+  function constructor() {
+    $scope.auth = Auth;
+    $scope.form = {};
+  }
 
-    API.SignIn.post({ username: form.username, password: form.password }, function (data) {
-      Auth.setAuth(data.user, data.token);
-      form.loading = false;
-      toaster.info("Signed in", "Welcome to the awesomeness " + form.username + ".");
-      $state.go("app.user", { username: form.username, user: Auth.getAuth() });
-
-    }, function (data) {
-      toaster.error("Unable to Sign in", "Incorrect username or password.");
-      form.loading = false;
-      form.error = data.data;
-    });
-  };
+  constructor();
 });
