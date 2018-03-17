bf5537000ea57cc5c4bafcff57908a23483d237d
Amir Savand <amir@savandbros.com>
2018-03-17 16:25:19 +0300

    Add url for FollowViewSet

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,19 @@ from rest_framework import routers
 from rest_framework.documentation import include_docs_urls
 from rest_framework_jwt.views import obtain_jwt_token
 
-from api.views import UserViewSet, UserCreateView, UserUpdateView, DeckViewSet, ActivityViewSet, CommentViewSet
+from api.views import (
+    UserViewSet,
+    FollowViewSet,
+    DeckViewSet,
+    ActivityViewSet,
+    CommentViewSet,
+    UserCreateView,
+    UserUpdateView,
+)
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'follows', FollowViewSet)
 router.register(r'decks', DeckViewSet)
 router.register(r'activities', ActivityViewSet)
 router.register(r'comments', CommentViewSet)
