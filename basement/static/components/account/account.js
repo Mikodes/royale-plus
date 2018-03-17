28bd8afbb008e57d6963574caa1488f27079f3c0
Amir Savand <amir@savandbros.com>
2018-03-17 17:35:23 +0300

    Add doc to Account

--- basement/static/components/account/account.js
+++ basement/static/components/account/account.js
@@ -3,12 +3,30 @@
 app.service("Account", function (ENV) {
   return function (data) {
 
+    /**
+     * @private
+     */
+    var self = this;
+
+    /**
+     * @private
+     * @type {string}
+     */
     var defaultImage = "static/assets/img/avatar.png?v=" + ENV.VERSION;
 
+    /**
+     * @type {object}
+     */
     this.get = data;
 
+    /**
+     * @type {string}
+     */
     this.username = this.get.username;
 
+    /**
+     * @type {string}
+     */
     this.picture = this.get.picture || defaultImage;
   };
 });
