c318fedc80fa341223dd37bdb89951034ece6e2b
Arsalan <arsalan@savandbros.com>
2018-03-19 21:03:48 +0300

    Add deck filtering

--- basement/static/components/deck/deck_list/deck_list_controller.js
+++ basement/static/components/deck/deck_list/deck_list_controller.js
@@ -1,10 +1,43 @@
 "use strict";
 
-app.controller("DeckListController", function (API, Pagination, Deck, toaster, $scope, $state, $stateParams) {
+app.controller("DeckListController", function (API, Main, Pagination, Deck, toaster, $scope, $state, $stateParams) {
 
   function constructor() {
 
     $scope.params = $stateParams;
+
+    /**
+     * Filter
+     *
+     * @type {object}
+     */
+    $scope.deckFilter = {
+      arena: -1,
+      kind: -1
+    };
+
+    /**
+     * Modes
+     *
+     * @type {Array<object>}
+     */
+    $scope.modes = [{
+      key: "mode_1v1",
+      label: "1v1"
+    }, {
+      key: "mode_2v2",
+      label: "2v2"
+    }, {
+      key: "mode_2x",
+      label: "2x Elixir"
+    }, {
+      key: "mode_3x",
+      label: "3x Elixir"
+    }];
+
+    angular.forEach($scope.modes, function (value) {
+      $scope.deckFilter[value.key] = false;
+    });
 
     /**
      * All decks to show
@@ -14,11 +47,25 @@ app.controller("DeckListController", function (API, Pagination, Deck, toaster, $
     $scope.decks = [];
 
     /**
+     * All areans
+     *
+     * @type {Array<string>}
+     */
+    $scope.deckArenas = Main.deck.arena;
+
+    /**
+     * All type of decks
+     *
+     * @type {Array<string>}
+     */
+    $scope.deckKinds = Main.deck.kind;
+
+    /**
      * APY payload
      *
      * @type {object}
      */
-    var payload = {
+    $scope.payload = {
       limit: 9
     };
 
@@ -26,14 +73,30 @@ app.controller("DeckListController", function (API, Pagination, Deck, toaster, $
      * If there's user in URL param, then update payload
      */
     if ($stateParams.id) {
-      payload.user = $stateParams.id;
+      $scope.payload.user = $stateParams.id;
     }
 
     /**
      * @type {Pagination}
      */
-    $scope.pagination = new Pagination(API.Decks.get, payload);
+    $scope.pagination = new Pagination(API.Decks.get, $scope.payload);
   }
+
+  $scope.filter = function (key) {
+
+    // Check for value
+    if ($scope.deckFilter[key] !== -1) {
+      $scope.payload[key] = $scope.deckFilter[key];
+    }
+
+    // Check for bool
+    if ($scope.deckFilter[key] == false) {
+      $scope.payload[key] = null;
+    }
+
+    $scope.decks = [];
+    $scope.pagination = new Pagination(API.Decks.get, $scope.payload);
+  };
 
   /**
    * Loaded decks
