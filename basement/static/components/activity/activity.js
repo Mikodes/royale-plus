5df738d6a582ab42a2221419108d0a9ecea7c00c
Amir Savand <amir@savandbros.com>
2018-03-22 15:24:22 +0300

    Add doc to Activity class

--- basement/static/components/activity/activity.js
+++ basement/static/components/activity/activity.js
@@ -3,19 +3,23 @@
 app.service("Activity", function (Main) {
   return function (data) {
 
-    var activityKindIcon = [
-      "flag",
-      "info",
-      "user-plus",
-      "cube"
-    ];
-
+    /**
+     * @type {object}
+     */
     this.get = data;
 
+    /**
+     * @type {function}
+     * @returns {string}
+     */
     this.getKind = function () {
       return Main.activity.kind[this.get.kind];
     };
 
+    /**
+     * @type {function}
+     * @returns {string}
+     */
     this.getLink = function () {
       if (this.getKind() == "User") {
         if (this.get.content.indexOf("commented") === 0) {
@@ -29,6 +33,10 @@ app.service("Activity", function (Main) {
       }
     };
 
+    /**
+     * @type {function}
+     * @returns {string}
+     */
     this.getIcon = function () {
       if (this.get.kind == 2) {
         if (this.get.content.indexOf("commented") === 0) {
@@ -43,6 +51,10 @@ app.service("Activity", function (Main) {
       return activityKindIcon[this.get.kind];
     };
 
+    /**
+     * @type {function}
+     * @returns {boolean}
+     */
     this.hasUser = function () {
       return this.getKind() === "User" || this.getKind() === "Deck";
     };
