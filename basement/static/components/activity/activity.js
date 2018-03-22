4f4f7cd08212a8c7e5c1fb976caf22a0052eefb0
Arsalan <arsalan@savandbros.com>
2018-03-22 15:06:45 +0300

    Fix documentation and add new function

--- basement/static/components/activity/activity.js
+++ basement/static/components/activity/activity.js
@@ -8,20 +8,43 @@ app.service("Activity", function (Main) {
       "info",
       "user-plus",
       "cube"
-    ]
+    ];
 
     this.get = data;
 
     this.getKind = function () {
       return Main.activity.kind[this.get.kind];
-    }
+    };
+
+    this.getLink = function () {
+      if (this.getKind() == "User") {
+        if (this.get.content.indexOf("commented") === 0) {
+
+        }
+        return "app.user({username: activity.get.issuer})";
+      }
+
+      if (this.getKind() == "Deck") {
+        return "app.deck({id: activity.get.issued})";
+      }
+    };
 
     this.getIcon = function () {
-      return activityKindIcon[this.get.kind]
-    }
+      if (this.get.kind == 2) {
+        if (this.get.content.indexOf("commented") === 0) {
+          return "comment";
+        }
+
+        if (this.get.content.indexOf("joined") === 0) {
+          return "user-plus ";
+        }
+      }
+
+      return activityKindIcon[this.get.kind];
+    };
 
     this.hasUser = function () {
       return this.getKind() === "User" || this.getKind() === "Deck";
-    }
+    };
   };
 });
