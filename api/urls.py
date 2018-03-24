2518b98afc2bdd303bd0b198accad3f85cfef64f
Amir Savand <amir@savandbros.com>
2018-03-24 17:43:04 +0300

    Improve code

--- api/urls.py
+++ api/urls.py
@@ -3,27 +3,19 @@ from rest_framework import routers
 from rest_framework.documentation import include_docs_urls
 from rest_framework_jwt.views import obtain_jwt_token
 
-from api.views import (
-    UserViewSet,
-    FollowViewSet,
-    DeckViewSet,
-    ActivityViewSet,
-    CommentViewSet,
-    UserCreateView,
-    UserUpdateView,
-)
+from api import views
 
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'follows', FollowViewSet)
-router.register(r'decks', DeckViewSet)
-router.register(r'activities', ActivityViewSet)
-router.register(r'comments', CommentViewSet)
+router.register(r'users', views.UserViewSet)
+router.register(r'follows', views.FollowViewSet)
+router.register(r'decks', views.DeckViewSet)
+router.register(r'activities', views.ActivityViewSet)
+router.register(r'comments', views.CommentViewSet)
 
 urlpatterns = router.urls
 urlpatterns += [
     url(r'^docs/', include_docs_urls(title='Royale Clan API'), name='docs'),
     url(r'^auth/', obtain_jwt_token, name='auth'),
-    url(r'^signup/', UserCreateView.as_view(), name='signup'),
-    url(r'^settings/', UserUpdateView.as_view(), name='settings'),
+    url(r'^signup/', views.UserCreateView.as_view(), name='signup'),
+    url(r'^settings/', views.UserUpdateView.as_view(), name='settings'),
 ]
